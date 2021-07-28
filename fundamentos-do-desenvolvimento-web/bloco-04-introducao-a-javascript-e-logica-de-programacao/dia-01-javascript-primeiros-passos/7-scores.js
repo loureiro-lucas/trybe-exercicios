1c1b7bba7933d8ea2950c7e7bde00ff2bb4239e7
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-07-28 19:37:19 -0300

    adds exercise 7

---
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/7-scores.js
@@ -0,0 +1,17 @@
+let percent = 75;
+
+if (percent > 100 || percent < 0) {
+  console.log ('error');
+} else if (percent >= 90) {
+  console.log ('A');
+} else if (percent >= 80) {
+  console.log ('B');
+} else if (percent >= 70) {
+  console.log ('C');
+} else if (percent >= 60) {
+  console.log ('D');
+} else if (percent >= 50) {
+  console.log ('E');
+} else {
+  console.log ('F');
+};
