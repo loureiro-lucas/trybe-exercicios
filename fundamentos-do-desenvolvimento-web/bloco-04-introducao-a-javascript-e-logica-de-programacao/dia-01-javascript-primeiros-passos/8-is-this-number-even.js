cc5988637d69ac7bfb5bf0b93ede45863d56abd7
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-07-28 19:48:01 -0300

    adds exercise 8

---
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/8-is-this-number-even.js
@@ -0,0 +1,12 @@
+let number1 = 5;
+let number2 = 13;
+let number3 = 15743;
+let even
+
+if (number1 % 2 == 0 || number2 % 2 == 0 || number3 % 2 == 0) {
+  even = true;
+} else {
+  even = false;
+};
+
+console.log (even);
