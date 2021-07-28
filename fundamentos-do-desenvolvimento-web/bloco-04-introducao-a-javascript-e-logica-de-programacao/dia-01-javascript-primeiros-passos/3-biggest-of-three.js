4480de3dd9c9e55d6b699ac5f80e7f0d326f4e52
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-07-28 18:49:52 -0300

    adds exercises 1, 2 and 3

---
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/3-biggest-of-three.js
@@ -0,0 +1,15 @@
+let a = 50;
+let b = 20;
+let c = 15;
+
+if (a>b) {
+  if (a>c) {
+    console.log(a);
+  } else {
+    console.log(c);
+  }
+} else if (b>c) {
+  console.log(b);
+} else {
+  console.log(c);
+};
