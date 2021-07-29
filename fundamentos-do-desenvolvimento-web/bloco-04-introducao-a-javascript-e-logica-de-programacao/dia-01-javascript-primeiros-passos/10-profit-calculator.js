68ba1e72e24a0849ff0a150682b9e6c5162d48ef
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-07-29 09:40:30 -0300

    improving exercice 10

--- fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/10-profit-calculator.js
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/10-profit-calculator.js
@@ -1,10 +1,12 @@
 let price = 1000;
 let cost = 500;
 
-profit = (price * 0.8) - cost
-
 if (price < 0 || cost < 0) {
   console.log('error');
 } else {
+  profit = (price * 0.8) - cost
   console.log (profit);
 };
+
+
+
