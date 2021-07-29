4f2f38580c5d7fe828a4b7abb15443fd8fab70ac
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-07-29 20:42:06 -0300

    adds bonus exercises

---
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/ex-bonus-03.js
@@ -0,0 +1,22 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbersMultiplied = [];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//     if (numbers[index] < numbers[secondIndex]) {
+//       let position = numbers[index];
+//       numbers[index] = numbers[secondIndex];
+//       numbers[secondIndex] = position;
+//     }
+//   }
+// }
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[(index + 1)] === undefined) {
+    numbersMultiplied.push(numbers[index] * 2);
+  } else {
+    numbersMultiplied.push(numbers[index] * numbers[(index +1)]);
+  }
+}
+
+console.log(numbersMultiplied);
