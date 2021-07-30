d4ca0e6e70efc424a02ee264a7dc88aace3111d1
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-07-30 18:16:26 -0300

    fixes exercise 1 and adds exercise 2

--- fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/ex-02.js
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/ex-02.js
@@ -4,8 +4,6 @@ let line = '';
 if (n>1) {
   for (let index = 1; index <= n; index += 1) {
     line += '*';
-  }
-  for (let index = 1; index <= n; index += 1) {
-    console.log(line);
+    console.log(line)
   }
 }
