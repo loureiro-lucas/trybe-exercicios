adac377ae9e146bf05928308669c78231d94d978
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-08-02 20:21:50 -0300

    adds part 2 exercise 2

---
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/ex-p2-02.js
@@ -0,0 +1,19 @@
+function compareNumbers(a, b) {
+  return a - b;
+}
+
+function biggestIndex(array) {
+  let biggest = "";
+  let index = "";
+  for (number of array) {
+    if (number>biggest) {
+      biggest = number;
+      index = array.indexOf(number);
+    }
+  }
+  return index;
+}
+
+array = [2, 3, 6, 7, 10, 1];
+
+console.log(biggestIndex(array));
