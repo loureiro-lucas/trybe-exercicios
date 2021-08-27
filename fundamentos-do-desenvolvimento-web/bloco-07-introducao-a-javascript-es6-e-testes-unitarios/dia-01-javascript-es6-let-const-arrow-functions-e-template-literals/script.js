7d14e50ad32f986b4307f3d750a8e9b58bb92708
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-08-27 19:40:48 -0300

    adds part 2 exercise 1

--- fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -13,19 +13,30 @@
 // testingScope(true);
 
 // EXERCÍCIO 2
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-const sorter = array => {
-  for (let index = 1; index < array.length; index += 1) {
-    for(let index2 = array.length -1; index2 > 0; index2 -= 1) {
-      let current = array[index2];
-      let previous = array[index2 - 1]
-      if (current < previous) {
-        array[index2] = previous;
-        array[index2 - 1] = current;
-      }
-    }
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// const sorter = array => {
+//   for (let index = 1; index < array.length; index += 1) {
+//     for(let index2 = array.length -1; index2 > 0; index2 -= 1) {
+//       let current = array[index2];
+//       let previous = array[index2 - 1]
+//       if (current < previous) {
+//         array[index2] = previous;
+//         array[index2 - 1] = current;
+//       }
+//     }
+//   }
+//   return array;
+// }
+
+// console.log(`Os números ${sorter(oddsAndEvens)} se encontram ordenados de forma crescente!`);
+
+//PARTE 2 - EXERCÍCIO 1
+const fatorial = n => {
+  let resultado = 1;
+  for(let index = n; index > 0; index -= 1) {
+    resultado *= index;
   }
-  return array;
+  return resultado;
 }
 
-console.log(`Os números ${sorter(oddsAndEvens)} se encontram ordenados de forma crescente!`);
+console.log(fatorial(0));
