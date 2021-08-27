df2c88156968b4b5962796ba941a631f10dba4a1
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-08-27 19:24:20 -0300

    adds exercise 2

--- fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,13 +1,31 @@
 // EXERCÍCIO 1
-const testingScope = escopo => {
-  if (escopo === true) {
-    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    const elseScope = `Não devo ser utilizada fora meu escopo (else)`;
-    console.log(elseScope);
+// const testingScope = escopo => {
+//   if (escopo === true) {
+//     let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     const elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+//     console.log(elseScope);
+//   }
+// }
+
+// testingScope(true);
+
+// EXERCÍCIO 2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const sorter = array => {
+  for (let index = 1; index < array.length; index += 1) {
+    for(let index2 = array.length -1; index2 > 0; index2 -= 1) {
+      let current = array[index2];
+      let previous = array[index2 - 1]
+      if (current < previous) {
+        array[index2] = previous;
+        array[index2 - 1] = current;
+      }
+    }
   }
+  return array;
 }
 
-testingScope(true);
+console.log(`Os números ${sorter(oddsAndEvens)} se encontram ordenados de forma crescente!`);
