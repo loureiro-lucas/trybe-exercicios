5a918e78fa31774fc9687620b610416d439af101
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-08-30 18:51:55 -0300

    adds part 2 exercise 1

---
+++ fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/part2.js
@@ -0,0 +1,24 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// EXERCISE 1
+const shiftAdder = (objectName, keyName, valueName) => objectName[keyName] = valueName;
+
+shiftAdder(lesson2, 'turno', 'manhã');
