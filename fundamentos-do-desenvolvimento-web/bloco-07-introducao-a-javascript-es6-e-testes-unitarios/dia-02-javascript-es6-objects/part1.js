7115e8a4cc501409415a503a39e9db97370ee884
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-08-30 18:41:57 -0300

    adds part 1 exercise 2

--- fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/part1.js
+++ fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/part1.js
@@ -35,14 +35,16 @@ const order = {
 };
 
 // EXERCÍCIO 1
-const customerInfo = (order) =>  `Olá, ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}.`;
+// const customerInfo = (order) =>  `Olá, ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}.`;
 
-console.log(customerInfo(order));
+// console.log(customerInfo(order));
 
 // EXERCÍCIO 2
 const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
-
+  order.name = 'Luiz Silva';
+  order.payment.total = 50;
+  const pizzas = Object.keys(order.order.pizza);
+  return `Olá, ${order.name}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${order.order.drinks.coke.type} é R$ ${order.payment.total},00.`
 }
 
-orderModifier(order);
+console.log(orderModifier(order));
