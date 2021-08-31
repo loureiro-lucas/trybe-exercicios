1070a0e4e180b349894fd874798398cd1301c35d
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-08-31 19:11:10 -0300

    adds part 1 exercise 2

--- fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/part1.js
+++ fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/part1.js
@@ -1,16 +1,36 @@
 // EXERCISE 1
+// const assert = require('assert');
+
+// function sum(a, b) {
+//   if (typeof a !== 'number' || typeof b !== 'number') {
+//     throw new Error('parameters must be numbers');
+//   }
+
+//   return a + b;
+// }
+
+// assert.strictEqual(sum(4, 5), 9);
+// assert.strictEqual(sum(0, 0), 0);
+// assert.throws(() => sum(4, '5'));
+// assert.throws(() => sum(4, '5'), /^Error: parameters must be numbers$/);
+
+// EXERCISE 2
 const assert = require('assert');
 
-function sum(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('parameters must be numbers');
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
   }
-
-  return a + b;
+  return newArr;
 }
 
-assert.strictEqual(sum(4, 5), 9);
-assert.strictEqual(sum(0, 0), 0);
-assert.throws(() => sum(4, '5'));
-assert.throws(() => sum(4, '5'), /^Error: parameters must be numbers$/);
-sum(4, '5')
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+const myArray = [1, 2, 3, 4];
+myRemove(myArray);
+assert.deepStrictEqual(myArray, [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+
