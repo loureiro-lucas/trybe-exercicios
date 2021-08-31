aef18dd6836179c1ea2c001ee16ad8659b9bc46d
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-08-31 20:16:00 -0300

    adds part 2 exercise 1

---
+++ fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/part2.js
@@ -0,0 +1,18 @@
+// EXERCISE 1
+const assert = require('assert');
+const addOne = array => {
+  let newArray = [];
+  for (item of array) {
+    newArray.push(item + 1);
+  }
+  return newArray;
+}
+
+const myArray = [31, 57, 12, 5];
+const unchanged = [31, 57, 12, 5];
+const expected = [32, 58, 13, 6];
+const output = addOne(myArray);
+
+assert.strictEqual(typeof addOne, 'function');
+assert.deepStrictEqual(output, expected);
+assert.deepStrictEqual(myArray, unchanged);
