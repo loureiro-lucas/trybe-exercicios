0acea606d7a075a84436b034493e1c65e2abe822
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-08-31 19:47:35 -0300

    adds part 1 exercise 3

--- fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/part1.js
+++ fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/part1.js
@@ -15,22 +15,43 @@
 // assert.throws(() => sum(4, '5'), /^Error: parameters must be numbers$/);
 
 // EXERCISE 2
+// const assert = require('assert');
+
+// function myRemove(arr, item) {
+//   let newArr = [];
+//   for (let index = 0; index < arr.length; index += 1) {
+//     if (item !== arr[index]) {
+//       newArr.push(arr[index]);
+//     }
+//   }
+//   return newArr;
+// }
+
+// assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+// assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+// const myArray = [1, 2, 3, 4];
+// myRemove(myArray);
+// assert.deepStrictEqual(myArray, [1, 2, 3, 4]);
+// assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+
+// EXERCISE 3
 const assert = require('assert');
 
-function myRemove(arr, item) {
-  let newArr = [];
-  for (let index = 0; index < arr.length; index += 1) {
-    if (item !== arr[index]) {
-      newArr.push(arr[index]);
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
     }
   }
-  return newArr;
+
+  return arr;
 }
 
-assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
-assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2 ,4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2 , 3, 4]);
 const myArray = [1, 2, 3, 4];
-myRemove(myArray);
-assert.deepStrictEqual(myArray, [1, 2, 3, 4]);
-assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
-
+myRemoveWithoutCopy(myArray, 3);
+assert.notDeepStrictEqual(myArray, [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
