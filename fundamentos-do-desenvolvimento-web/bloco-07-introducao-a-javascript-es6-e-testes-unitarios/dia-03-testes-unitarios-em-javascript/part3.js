80a5a4f1103da64294b300fa13078c0d7054bf39
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-08-31 21:09:15 -0300

    adds part 3 exercise 2

--- fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/part3.js
+++ fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/part3.js
@@ -1,18 +1,49 @@
 // EXERCISE 1
-const greetPeople = (people) => {
-  let finalGreeting = [];
-  for (const person in people) {
-    let greeting = 'Hello ';
-    greeting += people[person];
-    finalGreeting.push(greeting);
+// const greetPeople = (people) => {
+//   let finalGreeting = [];
+//   for (const person in people) {
+//     let greeting = 'Hello ';
+//     greeting += people[person];
+//     finalGreeting.push(greeting);
+//   }
+//   return finalGreeting;
+// };
+
+// const parameter = ['Irina', 'Ashleigh', 'Elsa'];
+// const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+
+// const assert = require('assert');
+
+// assert.strictEqual(typeof(greetPeople), 'function');
+// assert.deepStrictEqual(greetPeople(parameter), result);
+
+//EXERCISE 2
+const removeVowels = (word) => {
+  const characters = word.split('');
+  const results = [];
+  let number = 1;
+  for (let index = 0; index < characters.length; index += 1) {
+    if (
+      characters[index] === 'a' ||
+      characters[index] === 'o' ||
+      characters[index] === 'i' ||
+      characters[index] === 'e' ||
+      characters[index] === 'u'
+    ) {
+      results.push(number);
+      number += 1;
+    } else {
+      results.push(characters[index]);
+    }
   }
-  return finalGreeting;
+  return results.join('');
 };
 
-const parameter = ['Irina', 'Ashleigh', 'Elsa'];
-const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+
+const parameter = 'Dayane';
+const result = 'D1y2n3';
 
 const assert = require('assert');
 
-assert.strictEqual(typeof(greetPeople), 'function');
-assert.deepStrictEqual(greetPeople(parameter), result);
+assert.strictEqual(typeof(removeVowels), 'function');
+assert.strictEqual(removeVowels(parameter), result);
