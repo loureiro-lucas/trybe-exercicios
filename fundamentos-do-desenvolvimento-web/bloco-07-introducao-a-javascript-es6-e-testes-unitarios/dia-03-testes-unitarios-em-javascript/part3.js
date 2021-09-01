1d8e20ab77f6d3681494e8ea3362c654fab6ade4
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-08-31 21:12:15 -0300

    adds part 3 exercise 3

--- fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/part3.js
+++ fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/part3.js
@@ -18,32 +18,51 @@
 // assert.deepStrictEqual(greetPeople(parameter), result);
 
 //EXERCISE 2
-const removeVowels = (word) => {
-  const characters = word.split('');
-  const results = [];
-  let number = 1;
-  for (let index = 0; index < characters.length; index += 1) {
-    if (
-      characters[index] === 'a' ||
-      characters[index] === 'o' ||
-      characters[index] === 'i' ||
-      characters[index] === 'e' ||
-      characters[index] === 'u'
-    ) {
-      results.push(number);
-      number += 1;
-    } else {
-      results.push(characters[index]);
+// const removeVowels = (word) => {
+//   const characters = word.split('');
+//   const results = [];
+//   let number = 1;
+//   for (let index = 0; index < characters.length; index += 1) {
+//     if (
+//       characters[index] === 'a' ||
+//       characters[index] === 'o' ||
+//       characters[index] === 'i' ||
+//       characters[index] === 'e' ||
+//       characters[index] === 'u'
+//     ) {
+//       results.push(number);
+//       number += 1;
+//     } else {
+//       results.push(characters[index]);
+//     }
+//   }
+//   return results.join('');
+// };
+
+
+// const parameter = 'Dayane';
+// const result = 'D1y2n3';
+
+// const assert = require('assert');
+
+// assert.strictEqual(typeof(removeVowels), 'function');
+// assert.strictEqual(removeVowels(parameter), result);
+
+//EXERCISE 3
+const greaterThanTen = (array) => {
+  let results = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > 10) {
+      results.push(array[index]);
     }
   }
-  return results.join('');
+  return results;
 };
 
-
-const parameter = 'Dayane';
-const result = 'D1y2n3';
+const parameter = [4, 10, 32, 9, 21];
+const result = [32, 21];
 
 const assert = require('assert');
 
-assert.strictEqual(typeof(removeVowels), 'function');
-assert.strictEqual(removeVowels(parameter), result);
+assert.strictEqual(typeof(greaterThanTen), 'function');
+assert.deepStrictEqual(greaterThanTen(parameter), result);
