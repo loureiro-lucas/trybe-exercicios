ca349277e0f77120fe5b3a9c0288211976c0a4b6
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-09-03 09:17:41 -0300

    add bonus exercise part 2

--- fundamentos-do-desenvolvimento-web/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/ex_bonus.js
+++ fundamentos-do-desenvolvimento-web/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/ex_bonus.js
@@ -20,21 +20,56 @@ const dragon = {
 
 const battleMembers = { mage, warrior, dragon };
 
-const dragonDamage = dragon => Math.ramdom() * (dragon.strength - 15) + 15;
+const dragonDamage = () => Math.random() * (dragon.strength - 15) + 15;
 
-const warriorDamage = warrior => Math.ramdom() * ((warrior.strength * 2) - warrior.strength) + warrior.strength;
+const warriorDamage = () => Math.random() * ((warrior.strength * 2) - warrior.strength) + warrior.strength;
 
-const mageDamageAndMana = mage => {
+const mageDamageAndMana = () => {
   if (mage.mana < 15) {
     return {
       mageDamage: 'Not enough mana',
-      mageManaConcumption: 0,
-    }
+      mageManaConsumption: 0,
+    };
   }
   return {
   mageDamage: Math.random() * ((mage.intelligence * 2) - mage.intelligence) + mage.intelligence,
   mageManaConsumption: 15,
-  }
+  };
 }
 
-console.log(mageDamageAndMana(mage));
+const gameActions = {
+
+  warriorTurn: warriorDamage => {
+    warrior.damage = warriorDamage();
+    dragon.healthPoints -= warrior.damage;
+  },
+
+  mageTurn: mageDamageAndMana => {
+    const turn = mageDamageAndMana();
+    if (typeof turn.mageDamage === 'number') {
+      mage.damage = turn.mageDamage;
+      mage.mana -= turn.mageManaConsumption;
+      dragon.healthPoints -= mage.damage;
+    } else {
+      console.log(turn.mageDamage);
+    }
+  },
+
+  dragonTurn: dragonDamage => {
+    dragon.damage = dragonDamage();
+    warrior.healthPoints -= dragon.damage;
+    mage.healthPoints -= dragon.damage;
+  },
+
+  playTheGame: () => {
+    gameActions.warriorTurn(warriorDamage);
+    gameActions.mageTurn(mageDamageAndMana);
+    gameActions.dragonTurn(dragonDamage);
+    console.table(battleMembers)
+  },
+
+};
+
+while (warrior.healthPoints > 0 && dragon.healthPoints > 0) {
+  gameActions.playTheGame();
+};
