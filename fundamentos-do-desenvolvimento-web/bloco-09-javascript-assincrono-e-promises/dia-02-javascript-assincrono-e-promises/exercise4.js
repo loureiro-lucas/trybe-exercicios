331a1eb3cfe5dae5e92ea45284c1d9521dfe7303
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-09-17 09:54:32 -0300

    adds exercise 4

---
+++ fundamentos-do-desenvolvimento-web/bloco-09-javascript-assincrono-e-promises/dia-02-javascript-assincrono-e-promises/exercise4.js
@@ -0,0 +1,10 @@
+const promise = new Promise((resolve,reject) => {
+  const newArray = [];
+  for (let index = 1; index <= 10; index +=1) {
+    newArray.push((Math.floor((Math.random() * 50) + 1)) ** 2);
+  };
+  const sum = newArray.reduce((total, current) => total + current, 0);
+  sum > 8000 ? resolve(sum) : reject();
+})
+.then((sum) => [2, 3, 5, 10].map((number) => sum / number))
+.catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
