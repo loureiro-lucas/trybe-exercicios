140994252dac13e5f55afad7387e98ba093e8260
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-09-21 20:17:27 -0300

    exercise 4 done

--- fundamentos-do-desenvolvimento-web/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/exercise4.test.js
+++ fundamentos-do-desenvolvimento-web/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/exercise4.test.js
@@ -4,4 +4,16 @@ describe('test myFizzBuzz function', () => {
   it('test if myFizzBuzz with a number divisible by 3 and 5 returns fizzbuzz', () => {
     expect(myFizzBuzz(15)).toBe('fizzbuzz');
   });
+  it('test if myFizzBuzz with a number divisible by 3 fizz', () => {
+    expect(myFizzBuzz(9)).toBe('fizz');
+  });
+  it('test if myFizzBuzz with a number divisible by 5 returns buzz', () => {
+    expect(myFizzBuzz(10)).toBe('buzz');
+  });
+  it('test if myFizzBuzz with a number not divisible by 3 and/or 5 returns the number itself', () => {
+    expect(myFizzBuzz(7)).toBe(7);
+  });
+  it('test if myFizzBuzz with a number divisible by 3 and 5 returns fizzbuzz', () => {
+    expect(myFizzBuzz('x')).toBeFalsy();
+  });
 });
